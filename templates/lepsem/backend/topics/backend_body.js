cc48f7f0318a6cef6529b2826b60143f377be6f3
labby <rp@cms-lab.com>
2017-08-23 11:29:49 +0200

    some stylings

--- templates/lepsem/backend/topics/backend_body.js
+++ templates/lepsem/backend/topics/backend_body.js
@@ -12,10 +12,10 @@
 
 //  We try to set the semantic ui classes to some backend buttons
 var ref = document.getElementById("topics_save");
-if(ref) ref.setAttribute("class", "ui button green");
+if(ref) ref.setAttribute("class", "ui button positive");
 
 var ref = document.getElementById("topics_save_back");
-if(ref) ref.setAttribute("class", "ui button green");
+if(ref) ref.setAttribute("class", "ui button positive");
 
 var ref = document.getElementById("topics_cancel");
-if(ref) ref.setAttribute("class", "ui button red");
+if(ref) ref.setAttribute("class", "ui button negative");
