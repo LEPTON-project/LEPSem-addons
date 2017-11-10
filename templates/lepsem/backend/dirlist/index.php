376b8e2c8b2f22d5758d157ff1b3790820187c98
labby <rp@cms-lab.com>
2017-11-10 15:43:07 +0100

    add dirlist files

---
+++ templates/lepsem/backend/dirlist/index.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ *  @module         DirList
+ *  @version        see info.php of this module
+ *  @authors        Ralf Hertsch (†), cms-lab
+ *  @copyright      2007 - 2012 Ralf Hertsch (†)
+ *  @copyright      2013-2014 cms-lab 
+ *  @license        MIT License (MIT) http://www.opensource.org/licenses/MIT
+ *  @license terms  see info.php of this module
+ *
+ */
+
+// include class.secure.php to protect this file and the whole CMS!
+if ( defined( 'LEPTON_PATH' ) )
+{
+	include( LEPTON_PATH . '/framework/class.secure.php' );
+} //defined( 'LEPTON_PATH' )
+else
+{
+	$oneback = "../";
+	$root    = $oneback;
+	$level   = 1;
+	while ( ( $level < 10 ) && ( !file_exists( $root . '/framework/class.secure.php' ) ) )
+	{
+		$root .= $oneback;
+		$level += 1;
+	} //( $level < 10 ) && ( !file_exists( $root . '/framework/class.secure.php' ) )
+	if ( file_exists( $root . '/framework/class.secure.php' ) )
+	{
+		include( $root . '/framework/class.secure.php' );
+	} //file_exists( $root . '/framework/class.secure.php' )
+	else
+	{
+		trigger_error( sprintf( "[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER[ 'SCRIPT_NAME' ] ), E_USER_ERROR );
+	}
+}
+// end include class.secure.php
